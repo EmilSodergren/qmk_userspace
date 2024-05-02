7d54c7e13533b71d4e442340e360b2cb01084ded
Emil Sodergren <EmilSodergren@users.noreply.github.com>
2024-05-02 16:00:43 +0200

    Fix numpad and movement layer

--- keyboards/splitkb/aurora/lily58/rev1/keymaps/mre/keymap.c
+++ keyboards/splitkb/aurora/lily58/rev1/keymaps/mre/keymap.c
@@ -22,79 +22,89 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     //# |-------+-------+-------+-------+-------+-------|                                             |-------+-------+-------+-------+-------+-------|
     //# |   ⇧   |   A   |   S   |   D   |   F   |   G   |-------.                             .-------|   H   |   J   |   K   |   L   |   :   |   '   |
     //# |-------+-------+-------+-------+-------+-------|   [   |                             |   ]   |-------+-------+-------+-------+-------+-------|
-    //# |   ^   |   Z   |   X   |   C   |   V   |   B   |-------|                             |-------|   N   |   M   |   ,   |   .   |   /   |   ⇧   |
+    //# |   ^   |   Z   |   X   |   C   |   V   |   B   |-------|                             |-------|   N   |   M   |   ,   |   .   |   /   |   |   |
     //# .-------+-------+-------+-------+-------+-------/       /                             \       \-------+-------+-------+-------+-------+-------.
-    //#                      |   ⎇   |   ⌘   | MENT) | /       /                               \   ⏎   \  |   ⌫   | OLOR) | C_DEL |
+    //#                      |   ⎇   |   ⌘   | MENT) | /       /                               \   ⏎   \  |   ⌫   | IONS) |   ⌦   |
     //#                      |       |       |       |/       /                                 \       \ |       |       |       |
     //#                      .-------+-------+-------+-------.                                    .------++-------+-------+-------.
   [_BASE] = LAYOUT(
-      KC_GRV,   KC_1,     KC_2,     KC_3,    KC_4,    KC_5,                                  KC_6,    KC_7,       KC_8,    KC_9,     KC_0,     KC_MINS,
-      KC_TAB,   KC_Q,     KC_W,     KC_E,    KC_R,    KC_T,                                  KC_Y,    KC_U,       KC_I,    KC_O,     KC_P,     KC_EQL,
-      KC_LSFT,  KC_A,     KC_S,     KC_D,    KC_F,    KC_G,                                  KC_H,    KC_J,       KC_K,    KC_L,     KC_SCLN,  KC_QUOT,
-      KC_LCTL,  KC_Z,     KC_X,     KC_C,    KC_V,    KC_B,          KC_LBRC,      KC_RBRC,  KC_N,    KC_M,       KC_COMM, KC_DOT,   KC_SLSH,  KC_RSFT,
+      KC_GRV,   KC_1,     KC_2,     KC_3,    KC_4,    KC_5,                                  KC_6,    KC_7,           KC_8,    KC_9,     KC_0,     KC_MINS,
+      KC_TAB,   KC_Q,     KC_W,     KC_E,    KC_R,    KC_T,                                  KC_Y,    KC_U,           KC_I,    KC_O,     KC_P,     KC_EQL,
+      KC_LSFT,  KC_A,     KC_S,     KC_D,    KC_F,    KC_G,                                  KC_H,    KC_J,           KC_K,    KC_L,     KC_SCLN,  KC_QUOT,
+      KC_LCTL,  KC_Z,     KC_X,     KC_C,    KC_V,    KC_B,          KC_LBRC,      KC_RBRC,  KC_N,    KC_M,           KC_COMM, KC_DOT,   KC_SLSH,  KC_PIPE,
                                     KC_LALT, KC_LGUI, MO(_MOVEMENT), KC_SPC,       KC_ENT,   KC_BSPC, MO(_FUNCTIONS), KC_DEL
           ),
 
     //# .-----------------------------------------------.                                             .-----------------------------------------------.
     //# |       |       |       |       |       |       |                                             |       |       |       |       |       |       |
     //# |-------+-------+-------+-------+-------+-------|                                             |-------+-------+-------+-------+-------+-------|
-    //# | KC_F1 | KC_F2 | KC_F3 | KC_F4 | KC_F5 | KC_F6 |                                             | KC_F7 | KC_F8 | KC_F9 | C_F10 | C_F11 | C_F12 |
+    //# |       |       |       |       |       |       |                                             |       |   ⇟   |   ⇞   |       |       |       |
     //# |-------+-------+-------+-------+-------+-------|                                             |-------+-------+-------+-------+-------+-------|
-    //# |   `   |   !   |   @   |   #   |   $   |   %   |-------.                             .-------|   ←   |   ↓   |   ↑   |   →   |   ⇥   |   ~   |
+    //# |   `   |   !   |   @   |   #   |   $   |   %   |-------.                             .-------|   ←   |   ↓   |   ↑   |   →   |   ⇥   |       |
     //# |-------+-------+-------+-------+-------+-------|       |                             |   ⇤   |-------+-------+-------+-------+-------+-------|
-    //# |       |       |       |       |       |       |-------|                             |-------|   ⍉   |   _   |   +   |   {   |   }   |   |   |
+    //# |       |       |       |       |       |       |-------|                             |-------|   ⍉   |   _   |   +   |   {   |   }   |       |
     //# .-------+-------+-------+-------+-------+-------/       /                             \       \-------+-------+-------+-------+-------+-------.
     //#                      |       | MPAD) |       | /       /                               \       \  |       |   ⍉   |       |
     //#                      |       |       |       |/       /                                 \       \ |       |       |       |
     //#                      .-------+-------+-------+-------.                                    .------++-------+-------+-------.
   [_MOVEMENT] = LAYOUT(
       KC_TRANS, KC_TRANS, KC_TRANS, KC_TRANS, KC_TRANS,    KC_TRANS,                         KC_TRANS, KC_TRANS, KC_TRANS, KC_TRANS, KC_TRANS, KC_TRANS,
-      KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,       KC_F6,                            KC_F7,    KC_PGDN,    KC_PGUP,    KC_F10,   KC_F11,   KC_F12,
-      KC_GRV,   KC_EXLM,  KC_AT,    KC_HASH,  KC_DLR,      KC_PERC,                          KC_LEFT,  KC_DOWN,  KC_UP,    KC_RIGHT, KC_END,   KC_TILD,
-      KC_TRANS, KC_TRANS, KC_TRANS, KC_TRANS, KC_TRANS,    KC_TRANS, KC_TRANS,     KC_HOME,  KC_NO,    KC_UNDS,  KC_PLUS,  KC_LCBR,  KC_RCBR,  KC_PIPE,
+      KC_TRANS, KC_TRANS, KC_TRANS, KC_TRANS, KC_TRANS,    KC_TRANS,                         KC_TRANS, KC_PGDN,  KC_PGUP,  KC_TRANS, KC_TRANS, KC_TRANS,
+      KC_GRV,   KC_EXLM,  KC_AT,    KC_HASH,  KC_DLR,      KC_PERC,                          KC_LEFT,  KC_DOWN,  KC_UP,    KC_RIGHT, KC_END,   KC_TRANS,
+      KC_TRANS, KC_TRANS, KC_TRANS, KC_TRANS, KC_TRANS,    KC_TRANS, KC_TRANS,     KC_HOME,  KC_NO,    KC_UNDS,  KC_PLUS,  KC_LCBR,  KC_RCBR,  KC_TRANS,
                                     KC_TRANS, MO(_NUMPAD), KC_TRANS, KC_TRANS,     KC_TRANS, KC_TRANS, KC_NO,    KC_TRANS
           ),
 
     //# .-----------------------------------------------.                                             .-----------------------------------------------.
-    //# |       |       |       |       |       |       |                                             |       |       |       |       |       |       |
+    //# |       |       |       |       |       |       |                                             |   ⍉   |   7   |   8   |   9   |   /   |       |
     //# |-------+-------+-------+-------+-------+-------|                                             |-------+-------+-------+-------+-------+-------|
-    //# |   `   |   1   |   2   |   3   |   4   |   5   |                                             |   6   |   7   |   8   |   9   |   0   |       |
+    //# |   `   |   1   |   2   |   3   |   4   |   5   |                                             |   ⍉   |   4   |   5   |   6   |   *   |       |
     //# |-------+-------+-------+-------+-------+-------|                                             |-------+-------+-------+-------+-------+-------|
-    //# | KC_F1 | KC_F2 | KC_F3 | KC_F4 | KC_F5 | KC_F6 |-------.                             .-------|   ⍉   |   ←   |   ↓   |   ↑   | _RGHT |   ⍉   |
+    //# |   F1  |   F2  |   F3  |   F4  |   F5  |   F6  |-------.                             .-------|   ⍉   |   1   |   2   |   3   |   -   |       |
     //# |-------+-------+-------+-------+-------+-------|       |                             |       |-------+-------+-------+-------+-------+-------|
-    //# | KC_F7 | KC_F8 | KC_F9 | C_F10 | C_F11 | C_F12 |-------|                             |-------|   +   |   -   |   =   |   [   |   ]   |   \   |
+    //# |   F7  |   F8  |   F9  |  F10  |  F11  |  F12  |-------|                             |-------|   +   |   0   |   .   |   ,   |   +   |   ⏎   |
     //# .-------+-------+-------+-------+-------+-------/       /                             \       \-------+-------+-------+-------+-------+-------.
-    //#                      |       |       | MO(3) | /       /                               \       \  |       |       |       |
+    //#                      |       |       |       | /       /                               \       \  |       |       |       |
     //#                      |       |       |       |/       /                                 \       \ |       |       |       |
     //#                      .-------+-------+-------+-------.                                    .------++-------+-------+-------.
   [_NUMPAD] = LAYOUT(
-      KC_TRANS, KC_TRANS, KC_TRANS, KC_TRANS, KC_TRANS, KC_TRANS,                         KC_TRANS, KC_TRANS, KC_TRANS, KC_TRANS, KC_TRANS, KC_TRANS,
-      KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,                             KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_TRANS,
-      KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,                            KC_NO,    KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT,  KC_NO,
-      KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_TRANS,     KC_TRANS, KC_PLUS,  KC_MINS,  KC_EQL,   KC_LBRC,  KC_RBRC,  KC_BSLS,
-                                    KC_TRANS, KC_TRANS, MO(3),    KC_TRANS,     KC_TRANS, KC_TRANS, KC_TRANS, KC_TRANS
+      KC_TRANS, KC_TRANS, KC_TRANS, KC_TRANS, KC_TRANS, KC_TRANS,                         KC_NO,    KC_7,     KC_8,     KC_9,     KC_PSLS,  KC_TRANS,
+      KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,                             KC_NO,    KC_4,     KC_5,     KC_6,     KC_PAST,  KC_TRANS,
+      KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,                            KC_NO,    KC_1,     KC_2,     KC_3,     KC_PMNS,  KC_TRANS,
+      KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_TRANS,     KC_TRANS, KC_PLUS,  KC_0,     KC_DOT,   KC_COMM,  KC_PPLS,  KC_PENT,
+                                    KC_TRANS, KC_TRANS, KC_TRANS, KC_TRANS,     KC_TRANS, KC_TRANS, KC_TRANS, KC_TRANS
           ),
 
     //# .-----------------------------------------------.                                             .-----------------------------------------------.
-    //# |   ⍉   |   ⍉   |   ⍉   |   ⍉   |   ⍉   |   ⍉   |                                             |   ⍉   |   ⍉   |   ⍉   |   ⍉   |   ⍉   |   ⍉   |
+    //# |   ~   |   !   |   @   |   #   |   $   |   %   |                                             |   ^   |   &   |   *   |   (   |   )   |   _   |
     //# |-------+-------+-------+-------+-------+-------|                                             |-------+-------+-------+-------+-------+-------|
-    //# |   ⍉   |   ⍉   |   ⍉   |   ⍉   |   ⍉   |   ⍉   |                                             |   ⍉   |   ⍉   |   ⍉   |   ⍉   |   ⍉   |   ⍉   |
+    //# |       |   F1  |   F2  |   F3  |   F4  |   F5  |                                             |   F6  |   F7  |   F8  |   F9  |  F10  |       |
     //# |-------+-------+-------+-------+-------+-------|                                             |-------+-------+-------+-------+-------+-------|
-    //# |   ⍉   |   ⍉   |   ⍉   |   ⍉   |   ⍉   |   ⍉   |-------.                             .-------|   ⍉   |   ⍉   | B_TOG | B_HUI | B_SAI | B_VAI |
+    //# |       |  F11  |  F12  | B_HUI | B_SAI | B_VAI |-------.                             .-------|   ⍉   |   ⍉   |   ⍉   |   ⍉   |   ⍉   |       |
     //# |-------+-------+-------+-------+-------+-------|   ⍉   |                             |   ⍉   |-------+-------+-------+-------+-------+-------|
-    //# |   ⍉   |   ⍉   |   ⍉   |   ⍉   |   ⍉   |   ⍉   |-------|                             |-------|   ⍉   |   ⍉   | B_MOD | B_HUD | B_SAD | B_VAD |
+    //# |       | B_TOG | B_MOD | B_HUD | B_SAD | B_VAD |-------|                             |-------|   ⍉   |   ⍉   |   ⍉   |   ⍉   |   ⍉   |   ⍉   |
     //# .-------+-------+-------+-------+-------+-------/       /                             \       \-------+-------+-------+-------+-------+-------.
     //#                      |       |       |       | /       /                               \       \  |       |       |       |
     //#                      |       |       |       |/       /                                 \       \ |       |       |       |
     //#                      .-------+-------+-------+-------.                                    .------++-------+-------+-------.
   [_FUNCTIONS] = LAYOUT(
-      KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,                            KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,
-      KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,                            KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,
-      KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,                            KC_NO,    KC_NO,    RGB_TOG,  RGB_HUI,  RGB_SAI,  RGB_VAI,
-      KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,        KC_NO,    KC_NO,    KC_NO,    RGB_MOD,  RGB_HUD,  RGB_SAD,  RGB_VAD,
+      KC_TILD,  KC_EXLM,  KC_AT,    KC_HASH,  KC_DLR,   KC_PERC,                          KC_CIRC,  KC_AMPR,  KC_ASTR,  KC_LPRN,  KC_RPRN,  KC_UNDS,
+      KC_TRANS, KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,                            KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_TRANS,
+      KC_TRANS, KC_F11,   KC_F12,   RGB_HUI,  RGB_SAI,  RGB_VAI,                          KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_TRANS,
+      KC_TRANS, RGB_TOG,  RGB_MOD,  RGB_HUD,  RGB_SAD,  RGB_VAD,  KC_NO,        KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,
                                     KC_TRANS, KC_TRANS, KC_TRANS, KC_TRANS,     KC_TRANS, KC_TRANS, KC_TRANS, KC_TRANS
-          )
+          ),
+
 };
+/*
+  [15] = LAYOUT(
+      KC_TRANS, KC_TRANS, KC_TRANS, KC_TRANS, KC_TRANS, KC_TRANS,                         KC_TRANS, KC_TRANS, KC_TRANS, KC_TRANS, KC_TRANS, KC_TRANS,
+      KC_TRANS, KC_TRANS, KC_TRANS, KC_TRANS, KC_TRANS, KC_TRANS,                         KC_TRANS, KC_TRANS, KC_TRANS, KC_TRANS, KC_TRANS, KC_TRANS,
+      KC_TRANS, KC_TRANS, KC_TRANS, KC_TRANS, KC_TRANS, KC_TRANS,                         KC_TRANS, KC_TRANS, KC_TRANS, KC_TRANS, KC_TRANS, KC_TRANS,
+      KC_TRANS, KC_TRANS, KC_TRANS, KC_TRANS, KC_TRANS, KC_TRANS, KC_TRANS,     KC_TRANS, KC_TRANS, KC_TRANS, KC_TRANS, KC_TRANS, KC_TRANS, KC_TRANS,
+                                    KC_TRANS, KC_TRANS, KC_TRANS, KC_TRANS,     KC_TRANS, KC_TRANS, KC_TRANS, KC_TRANS
+         )
+*/
 
 /*
 qmk-keyboard-format:json:begin
@@ -178,6 +188,18 @@ qmk-keyboard-format:json:begin
         "KC_X": "  X  " ,
         "KC_Y": "  Y  " ,
         "KC_Z": "  Z  " ,
+        "KC_F1": "  F1 " ,
+        "KC_F2": "  F2 " ,
+        "KC_F3": "  F3 " ,
+        "KC_F4": "  F4 " ,
+        "KC_F5": "  F5 " ,
+        "KC_F6": "  F6 " ,
+        "KC_F7": "  F7 " ,
+        "KC_F8": "  F8 " ,
+        "KC_F9": "  F9 " ,
+        "KC_F10": " F10 " ,
+        "KC_F11": " F11 " ,
+        "KC_F12": " F12 " ,
         "KC_COMMA": "  ,  ",
         "KC_COMM": "  ,  ",
         "KC_DOT": "  .  ",
@@ -185,6 +207,7 @@ qmk-keyboard-format:json:begin
         "KC_SCLN": "  :  ",
         "KC_SLASH": "  \/  ",
         "KC_SLSH": "  \/  ",
+        "KC_PSLS": "  \/  ",
         "KC_BSLS": "  \\  ",
         "KC_EXLM": "  !  ",
         "KC_PIPE": "  |  ",
@@ -199,13 +222,16 @@ qmk-keyboard-format:json:begin
         "KC_EQUAL": "  =  ",
         "KC_ASTR": "  *  ",
         "KC_KP_ASTERISK": "  *  ",
+        "KC_PAST": "  *  ",
         "KC_LABK": "  <  ",
         "KC_RABK": "  >  ",
         "KC_BSLASH": "  \\  ",
         "KC_MINS": "  -  ",
         "KC_MINUS": "  -  ",
+        "KC_PMNS": "  -  ",
         "KC_UNDS": "  _  ",
         "KC_PLUS": "  +  ",
+        "KC_PPLS": "  +  ",
         "KC_LCBR": "  {  ",
         "KC_RCBR": "  }  ",
         "KC_LPRN": "  (  ",
@@ -239,14 +265,15 @@ qmk-keyboard-format:json:begin
         "KC_PAUSE": "  ⎉  " ,
         "KC_BREAK": "  ⎊  " ,
         "KC_ENT": "  ⏎  " ,
+        "KC_PENT": "  ⏎  " ,
         "KC_BSPC": "  ⌫  " ,
         "KC_SPC": "     " ,
-        "KC_DELETE": "  ⌦  " ,
+        "KC_DEL": "  ⌦  " ,
         "KC_INSERT": "  ⎀  " ,
         "KC_HOME": "  ⇤  " ,
         "KC_END": "  ⇥  " ,
         "KC_PGUP": "  ⇞  " ,
-        "KC_PGDOWN": "  ⇟  " ,
+        "KC_PGDN": "  ⇟  " ,
         "KC_LSFT": "  ⇧  " ,
         "KC_RSFT": "  ⇧  " ,
         "KC_LCTL": "  ^  " ,
